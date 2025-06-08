9bca6bcb9e7fd29a92a58235c74e9801adb902bb
Nilakshi1223 <nmiyurangika1223@gmail.com>
2025-06-08 18:55:20 +0530

    Springboot-basics-commit1

---
+++ src/test/java/com/springBootBasics/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBootBasics.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
